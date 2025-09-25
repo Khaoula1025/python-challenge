52922eecd49c44a90486c0f5a2e8c724081d945c
Khaoula Esioudi <khaoula.esioudi@gmail.com>
2025-09-25 15:01:11 +0100

    added bonus question

--- Main.py
+++ Main.py
@@ -1,7 +1,13 @@
 import data as d 
 import utils
 
-#print('association de chaque produits a son prix :',utils.association(d))
-#print('les produit dont le prix est superieur a 30 :',utils.filtered(d))
-#utils.transformation(d)
-print('trier par ordre de  croissant de prix :',utils.ordreCroissantDesPrix(d))
+# print('association de chaque produits a son prix :',utils.association(d))
+# print('les produit dont le prix est superieur a 30 :',utils.filtered(d))
+# print(utils.transformation(d))
+# print('trier par ordre de  croissant de prix :',utils.ordreCroissantDesPrix(d))
+# print('le produit le plus cher est : ',utils.plusCher(d))
+# print('le produit le moin  cher est : ',utils.moinsCher(d))
+# utils.affichageAvecMention(d)
+print(utils.affichageAvecMention1(d))
+
+
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import data as d 
 
 def association(n):
     association=list(zip(n.produits,n.prix))
@@ -11,12 +12,35 @@ def filtered(n):
 
 def transformation(n):
     for i in association(n):
-        print(f"{i[0]} coute {i[1]} dh")
+        print(i)
+        return f"{i[0]} coute {i[1]} dh"
 
 def ordreCroissantDesPrix(n):
-    def sortn(n):
-        return n[1] 
-    #association(n).sort(key=lambda x:x[1])
-    association(n).sort(key=sortn)
-    return association(n)
-    
+    assoc=association(n)
+    assoc.sort(key=lambda x:x[1])
+    return  assoc
+
+def plusCher(n):
+    return ordreCroissantDesPrix(n)[-1] 
+def moinsCher(n):
+    return ordreCroissantDesPrix(n)[0] 
+
+def affichageAvecMention(n):
+    for i in association(n):
+        x =f"{i[0]} coute {i[1]} dh"
+        if i[1]>1000:
+            x+='(LUXE)'
+        print(x)
+def addLuxe(n):
+       if n[1]>1000:
+            x =f"{n[0]} coute {n[1]} dh LUXE"
+       else:
+            x =f"{n[0]} coute {n[1]} dh"
+       return x
+def affichageAvecMention1(n):
+    return list(map(addLuxe,association(n)))
+
+def affichageAvecMention2(n):
+    assoc=association(n)
+    return list(map(lambda x:f"{x[0]} coute {x[1]} dh LUXE" if x[1] >= 2000 else f"{x[0]} coute {x[1]} dh",assoc))
+
