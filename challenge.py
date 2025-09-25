1e354d7eeef1b41005813c0688820a6242ac8161
Khaoula Esioudi <khaoula.esioudi@gmail.com>
2025-09-25 09:54:41 +0100

    added filter function

--- challenge.py
+++ challenge.py
@@ -1,4 +1,8 @@
 import data as d
+#Association
+association=list(zip(d.produits,d.prix))
+print('association de chaque produits a son prix : ',association)
 
-association=zip(d.produits,d.prix)
-print('association de chaque produits a son prix : ',list(association))
+#Filter
+filterd=list(filter(lambda x:x[1]>=30,association))
+print('les produit dont le prix est superieur a 30 : ',filterd)
