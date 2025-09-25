faff4df5a4ee5c1717175a783742a6a1591e49e3
Khaoula Esioudi <khaoula.esioudi@gmail.com>
2025-09-25 11:31:41 +0100

    added transformation

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 import data as d 
 import utils
 
-print('association de chaque produits a son prix :',utils.association(d))
-print('les produit dont le prix est superieur a 30 :',utils.filtered(d))
+#print('association de chaque produits a son prix :',utils.association(d))
+#print('les produit dont le prix est superieur a 30 :',utils.filtered(d))
+#utils.transformation(d)
+print('trier par ordre de  croissant de prix :',utils.ordreCroissantDesPrix(d))
--- utils.py
+++ utils.py
@@ -2,11 +2,21 @@
 def association(n):
     association=list(zip(n.produits,n.prix))
     return association
+def price_overTherty(a):
+    return a[1]>=30
 
 def filtered(n):
-    filterd=list(filter(lambda x:x[1]>=30,association(n)))
+    filterd=list(filter(price_overTherty,association(n)))
     return filterd
 
 def transformation(n):
     for i in association(n):
         print(f"{i[0]} coute {i[1]} dh")
+
+def ordreCroissantDesPrix(n):
+    def sortn(n):
+        return n[1] 
+    #association(n).sort(key=lambda x:x[1])
+    association(n).sort(key=sortn)
+    return association(n)
+    
