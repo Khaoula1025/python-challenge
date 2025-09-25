6d80f9b91d6ac0f525bf500b75a562940ffbac17
Khaoula Esioudi <khaoula.esioudi@gmail.com>
2025-09-25 09:46:52 +0100

    defined data and association

---
+++ challenge.py
@@ -0,0 +1,4 @@
+import data as d
+
+association=zip(d.produits,d.prix)
+print('association de chaque produits a son prix : ',list(association))
---
+++ data.py
@@ -0,0 +1,2 @@
+produits=['Clavier','Souris','Ecran','chaise','Bureau','lampe']
+prix=[45,150,1200,855,2000,25]
